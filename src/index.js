b72fe8ae23a53db1a6ff12db3af3b3540d1110ca
Blick7 <dima.8m8@gmail.com>
2021-03-12 11:07:35 +0200

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let min;
+  if (!Array.isArray(array) || array.length === 0)
+    return 0;
+  else {
+    min = array[0];
+    for (let item of array) {
+      if (item < min)
+        min = item;
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let max;
+  if (!Array.isArray(array) || array.length === 0)
+    return 0;
+  else {
+    max = array[0];
+    for (let item of array) {
+      if (item > max)
+        max = item;
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let avg = 0;
+  if (!Array.isArray(array) || array.length === 0)
+    return 0;
+  else {
+    for (let item of array)
+      avg += item;
+  }
+  return avg / array.length;
 }
